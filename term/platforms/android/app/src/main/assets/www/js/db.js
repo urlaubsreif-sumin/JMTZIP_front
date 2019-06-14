0ce403f40b40d25a02e0ea9cfaa0727f8cc13396
LEE-SUMIN <sumini0715@naver.com>
2019-06-15 03:23:14 +0900

    commit 6/15 3:23
    commit 6/15 3:23

--- term/platforms/android/app/src/main/assets/www/js/db.js
+++ term/platforms/android/app/src/main/assets/www/js/db.js
@@ -1,31 +1,27 @@
-var db = window.openDatabase("mainDB", 1.0, "mainDB_for_matzip",200000)
+var db = window.openDatabase("TestDB", "1.0", "this is for test", 200000);
 
-function createDB(){
-    db.transaction(function(tx){
-        alert('create!');
-        tx.executeSql("CREATE TABLE matzip(ID, name, category, address, location, time, cost, score, personnel)");
-    });
-}
+    function createTable(){
+        db.transaction(function(tx){
+            tx.executeSql('CREATE TABLE IF NOT EXISTS testTable (name, score)');
+        });
+    }
 
-function errorCB(err) {
-        alert("Error processing SQL: "+err.code);
-}
+    function insertData(){
+        db.transaction(function(tx){
+            tx.executeSql("insert into testTable values(?,?)", [txtName.value, txtScore.value]);
+        });
+    }
 
-function insertData(){
-    db.transaction(function(tx){
-        tx.executeSql("INSERT INTO matzip VALUES(?,?), [txtName.value, txtScore.value]")
-    });
-}
+    function selectData(){
+        db.transaction(function(tx){
+            tx.executeSql("select * from testTable", [], successCB);
+        });
+    }
 
-function selectData(){
-    db.transaction(function(tx){
-        tx.executeSql("SELECT * FROM matzip", [],
-            function(tx, result){
-                for(var i = 0; i < result.rows.length; i++){
-                    var row = result.rows.item(i);
-                    document.getElementById('resultTable').innerHTML += "<tr><td>" + row['name'] + "</td><td>" + row['score'] + "</td><td></td></tr>" + ;
-                }
-            }
-        );
-    });
-}
+    function successCB(tx, result){
+        document.getElementById('resultTable').innerHTML = "";
+        for(var i = 0; i < result.rows.length; i++){
+            var row = result.rows.item(i);
+            document.getElementById('resultTable').innerHTML += "<tr><td>" + row['name'] + "</td><td>" + row['score'] + "</td><td></td></tr>";
+        }
+    }
