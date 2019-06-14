89ce53141234e854e0831584a5f976c6f9841e94
LEE-SUMIN <sumini0715@naver.com>
2019-06-15 08:20:12 +0900

    commit 06/15 8:20
    commit 06/15 8:20

--- term/platforms/android/app/src/main/assets/www/js/db.js
+++ term/platforms/android/app/src/main/assets/www/js/db.js
@@ -1,37 +1,27 @@
-var db = window.openDatabase("testDb.db");
+var db = window.openDatabase("TestDB", "1.0", "this is for test", 200000);
 
-function createTable(){
-    db.transaction(function(tx){
-        tx.executeSql('CREATE TABLE IF NOT EXISTS testTable (name, score)');
-    });
-}
-
-function insertData(){
-    db.transaction(function(tx){
-        tx.executeSql("INSERT INTO testTable values(?,?)", [txtName.value, txtScore.value]);
-    });
-}
-
-function selectData(){
-    db.transaction(function(tx){
-        tx.executeSql("SELECT * FROM testTable", [], successCB);
-    });
-}
-
-<<<<<<< HEAD
-function successCB(tx, result){
-    document.getElementById('resultTable').innerHTML = "<th>가게 명</th><th>평점</th><th>자세히</th>";
-    for(var i = 0; i < result.rows.length; i++){
-        var row = result.rows.item(i);
-        document.getElementById('resultTable').innerHTML += "<tr><td>" + row['name'] + "</td><td>" + row['score'] + "</td><td></td></tr>";
+    function createTable(){
+        db.transaction(function(tx){
+            tx.executeSql('CREATE TABLE IF NOT EXISTS testTable (name, score)');
+        });
     }
-}
-=======
+
+    function insertData(){
+        db.transaction(function(tx){
+            tx.executeSql("insert into testTable values(?,?)", [txtName.value, txtScore.value]);
+        });
+    }
+
+    function selectData(){
+        db.transaction(function(tx){
+            tx.executeSql("select * from testTable", [], successCB);
+        });
+    }
+
     function successCB(tx, result){
-        document.getElementById('resultTable').innerHTML = "";
+        document.getElementById('resultTable').innerHTML = "<th>가게 명</th><th>평점</th><th>자세히</th>";
         for(var i = 0; i < result.rows.length; i++){
             var row = result.rows.item(i);
             document.getElementById('resultTable').innerHTML += "<tr><td>" + row['name'] + "</td><td>" + row['score'] + "</td><td></td></tr>";
         }
     }
->>>>>>> parent of a48b4e2... commit 6/15 3:30
--- term/platforms/android/platform_www/cordova_plugins.js
+++ term/platforms/android/platform_www/cordova_plugins.js
@@ -369,14 +369,6 @@ module.exports = [
       "navigator.notification",
       "navigator"
     ]
-  },
-  {
-    "id": "cordova-sqlite-storage.SQLitePlugin",
-    "file": "plugins/cordova-sqlite-storage/www/SQLitePlugin.js",
-    "pluginId": "cordova-sqlite-storage",
-    "clobbers": [
-      "SQLitePlugin"
-    ]
   }
 ];
 module.exports.metadata = 
@@ -392,8 +384,7 @@ module.exports.metadata =
   "cordova-plugin-globalization": "1.0.9",
   "cordova-plugin-inappbrowser": "1.7.2",
   "cordova-plugin-network-information": "1.3.4",
-  "cordova-plugin-vibration": "2.1.6",
-  "cordova-sqlite-storage": "3.2.0"
+  "cordova-plugin-vibration": "2.1.6"
 };
 // BOTTOM OF METADATA
 });
