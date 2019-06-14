eb6127d3aff3f2267781685cc3cb627f53a5eecf
LEE-SUMIN <sumini0715@naver.com>
2019-06-15 02:52:43 +0900

    commit 06/15 2:52
    commit 06/15 2:52

--- term/platforms/android/app/src/main/assets/www/js/db.js
+++ term/platforms/android/app/src/main/assets/www/js/db.js
@@ -1,44 +1,31 @@
-var db = openDatabase("mainDB", 1.0, "mainDB_for_matzip",200000)
+var db = window.openDatabase("mainDB", 1.0, "mainDB_for_matzip",200000)
 
-function createTable(){
+function createDB(){
     db.transaction(function(tx){
-        tx.executeSql("CREATE TABLE mainDB(name, category, address, location, time, cost, score, personnel)");
+        alert('create!');
+        tx.executeSql("CREATE TABLE matzip(ID, name, category, address, location, time, cost, score, personnel)");
     });
+}
+
+function errorCB(err) {
+        alert("Error processing SQL: "+err.code);
 }
 
 function insertData(){
     db.transaction(function(tx){
-        tx.executeSql("INSERT INTO mainDB VALUES(?,?,?,?,?,?,?,?), [txtName.value, txtCategory.value, txtAddress.value, txtLocation.value, txtTime.value, txtCost.value, txtScore.value, txtPersonnel.value]")
+        tx.executeSql("INSERT INTO matzip VALUES(?,?), [txtName.value, txtScore.value]")
     });
 }
 
-
 function selectData(){
     db.transaction(function(tx){
-        tx.executeSql("SELECT * FROM MainDB")
-    })
-    }
+        tx.executeSql("SELECT * FROM matzip", [],
+            function(tx, result){
+                for(var i = 0; i < result.rows.length; i++){
+                    var row = result.rows.item(i);
+                    document.getElementById('resultTable').innerHTML += "<tr><td>" + row['name'] + "</td><td>" + row['score'] + "</td><td></td></tr>" + ;
+                }
+            }
+        );
+    });
 }
-}
-
-function selectData(){
-
-        db.transaction(function(tx){
-
-          tx.executeSql("select * from Test",[],
-
-
-
-            function(tx,result){
-
-              for(var i = 0; i < result.rows.length; i++){
-
-                var row = result.rows.item(i);
-
-                document.getElementById('table1').innerHTML +=  "<tr><td>" + row['id'] + "</td><td>" + row['name'] + "</td></tr>";
-
-              }
-
-
-
-            });
