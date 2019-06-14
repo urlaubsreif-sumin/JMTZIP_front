1e503260bf81a5256038f74e2ad5338f7fa426ce
LEE-SUMIN <sumini0715@naver.com>
2019-06-15 05:14:02 +0900

    commit 6/15 5:13
    commit 6/15 5:13

--- term/platforms/android/app/src/main/assets/www/js/db.js
+++ term/platforms/android/app/src/main/assets/www/js/db.js
@@ -1,4 +1,4 @@
-var db = window.openDatabase("TestDB", "1.0", "this is for test", 200000);
+var db = window.openDatabase("testDb.db");
 
 function createTable(){
     db.transaction(function(tx){
@@ -18,10 +18,10 @@ function selectData(){
     });
 }
 
-    function successCB(tx, result){
-        document.getElementById('resultTable').innerHTML = "<th>가게 명</th><th>평점</th><th>자세히</th>";
-        for(var i = 0; i < result.rows.length; i++){
-            var row = result.rows.item(i);
-            document.getElementById('resultTable').innerHTML += "<tr><td>" + row['name'] + "</td><td>" + row['score'] + "</td><td></td></tr>";
-        }
+function successCB(tx, result){
+    document.getElementById('resultTable').innerHTML = "<th>가게 명</th><th>평점</th><th>자세히</th>";
+    for(var i = 0; i < result.rows.length; i++){
+        var row = result.rows.item(i);
+        document.getElementById('resultTable').innerHTML += "<tr><td>" + row['name'] + "</td><td>" + row['score'] + "</td><td></td></tr>";
     }
+}
