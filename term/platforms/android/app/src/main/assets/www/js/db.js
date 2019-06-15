b3953f50858cb8e9d9fec368a10e1dbe59639ad4
LEE-SUMIN <sumini0715@naver.com>
2019-06-15 14:30:26 +0900

    commit 6/15 14:30
    commit 6/15 14:30

--- term/platforms/android/app/src/main/assets/www/js/db.js
+++ term/platforms/android/app/src/main/assets/www/js/db.js
@@ -1,27 +1,39 @@
-var db = window.openDatabase("TestDB", "1.0", "this is for test", 200000);
+document.addEventListener("deviceready", onDeviceReady, false);
+var db;
+function onDeviceReady() {
+    alert('start!');
+    db = window.sqlitePlugin.openDatabase({name: "testDB", location: "default", androidDatabaseProvider: 'system'});
+    alert('db set!');
+}
 
-    function createTable(){
-        db.transaction(function(tx){
-            tx.executeSql('CREATE TABLE IF NOT EXISTS testTable (name, score)');
-        });
-    }
+function createTable(){
+    db.transaction(function(tx){
+         tx.executeSql('CREATE TABLE IF NOT EXISTS testTable (name, score)', [],
+              function(tx, result) {
+                   alert("success!!");
+              },
+              function(error) {
+                   alert("failed");
+              });
+    });
+}
 
-    function insertData(){
-        db.transaction(function(tx){
-            tx.executeSql("insert into testTable values(?,?)", [txtName.value, txtScore.value]);
-        });
-    }
+function insertData(){
+    db.transaction(function(tx){
+         tx.executeSql("INSERT INTO testTable VALUES(?,?)", [txtName.value, txtScore.value]);
+    });
+}
 
-    function selectData(){
-        db.transaction(function(tx){
-            tx.executeSql("select * from testTable", [], successCB);
-        });
-    }
+function selectData(){
+    db.transaction(function(tx){
+         tx.executeSql("SELECT * FROM testTable", [], successCB);
+    });
+}
 
-    function successCB(tx, result){
-        document.getElementById('resultTable').innerHTML = "<th>가게 명</th><th>평점</th><th>자세히</th>";
-        for(var i = 0; i < result.rows.length; i++){
-            var row = result.rows.item(i);
-            document.getElementById('resultTable').innerHTML += "<tr><td>" + row['name'] + "</td><td>" + row['score'] + "</td><td></td></tr>";
-        }
+function successCB(tx, res){
+    document.getElementById('resultTable').innerHTML = "<th>가게 명</th><th>평점</th><th>자세히</th>";
+    for(var i = 0; i < res.rows.length; i++){
+          var row = res.rows.item(i);
+          document.getElementById('resultTable').innerHTML += "<tr><td>" + row['name'] + "</td><td>" + row['score'] + "</td><td></td></tr>";
     }
+}
